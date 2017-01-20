c04f7bad72fb2c9ed5245f6a48b820ff5d2ee715
Bruce Dae-Yeong Kim <dykim1983@gmail.com>
2017-01-20 10:26:26 +0900

    support  hal-browser + querydsl

--- src/main/java/com/dykim1983/integration/repository/MemberRepository.kt
+++ src/main/java/com/dykim1983/integration/repository/MemberRepository.kt
@@ -1,8 +1,10 @@
 package com.dykim1983.integration.repository
 
 import com.dykim1983.integration.domain.Member
+import com.dykim1983.integration.domain.QMember
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.querydsl.QueryDslPredicateExecutor
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 
 @RepositoryRestResource(collectionResourceRel = "member", path = "member")
-interface MemberRepository : JpaRepository<Member, Long>
+interface MemberRepository : JpaRepository<Member, Long>, QueryDslPredicateExecutor<QMember>
